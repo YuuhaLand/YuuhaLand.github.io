cf50ff29fb64d0bc5893816d4bf38698de878b55
ごて優香 Yuka0745 <78265558+Gotemba912@users.noreply.github.com>
2023-07-20 18:06:56 +0900

    ｆヴぇ

---
+++ minecraft_addons/z_addon_list.js
@@ -0,0 +1,132 @@
+const adlist = {
+	addon_list: [
+		{
+			name: "Gote-Supporter",
+			description: "マイクラを快適に便利に楽しめるように設計されたUIリソースパックです!",
+			icon_url: "../images/gote-supporter.png",
+			github_counter: "gote-supporter",
+			github_plus: 127696,
+			link: "./gote-supporter.html"
+		},
+		{
+			name: "Hack Item Giver",
+			description: "Hackなどを使わずに通常では出せないアイテムやブロックを出せるようにするアドオンです!",
+			icon_url: "../images/hack_item_giver.png",
+			github_counter: "",
+			github_plus: 0,
+			link: "./hack_item_giver.html"
+		},
+		{
+			name: "Custom UI Maker",
+			description: "本と羽根ペンを使って内容を自由に入力して表示できるUIを作ることができます!",
+			icon_url: "../images/custom_ui_maker.png",
+			github_counter: "",
+			github_plus: 0,
+			link: "./custom_ui_maker.html"
+		},
+		{
+			name: "Legacy GameConsoleUI",
+			description: "コンソールエディション(WiiU, 旧Switch, PS3, 旧PS4, Xbox360, 旧Xbox One)のようなUIにするリソースパックです!",
+			icon_url: "./images/consoleUI.png",
+			github_counter: "consoleui",
+			github_plus: 0,
+			link: "./console_ui.html"
+		},
+		{
+			name: "Chat Text Copy",
+			description: "チャットの文章を簡単にコピペできます",
+			icon_url: "https://github.com/Gotemba912/chat-text-copy/blob/main/pack_icon.png?raw=true",
+			github_counter: "chat-text-copy",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/chat-text-copy/releases/latest"
+		},
+		{
+			name: "Preview デバッグテキスト切り替え",
+			description: "Preview版の上のデバッグテキストを非表示にしたりできます。<br>HUD画面でF10を押して切り替えます。",
+			icon_url: "https://github.com/Gotemba912/debug_text_toggle/blob/main/pack_icon.png?raw=true",
+			github_counter: "debug_text_toggle",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/debug_text_toggle/releases/latest"
+		},
+		{
+			name: "タッチ操作でかんたんF5",
+			description: "F5キーをスマホで押せる！<br>camera コマンドの実験を有効化する必要があります。",
+			icon_url: "https://github.com/Gotemba912/touch-f5/blob/main/resource/pack_icon.png?raw=true",
+			github_counter: "touch-f5",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/touch-f5/releases/latest"
+		},
+		{
+			name: "Custom boss bar UI",
+			description: "カスタムボスバーをscoreboardのsidebarで作れます。<br>/scoreboard objectives add &lt;ID&gt; dummy",
+			icon_url: "https://github.com/Gotemba912/Custom-bossbar-addon/blob/main/pack_icon.png?raw=true",
+			github_counter: "Custom-bossbar-addon",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/Custom-bossbar-addon/releases/latest"
+		},
+		{
+			name: "Fullscreen Bock Screen",
+			description: "Gote-Supporterにあったフルスクリーンの本画面です。",
+			icon_url: "./images/fullscreen_bock_icon.png",
+			github_counter: "fullscreen-bock-screen",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/fullscreen-bock-screen/releases/latest"
+		},
+		{
+			name: "Fullscreen Play Screen",
+			description: "Gote-Supporterにあったフルスクリーンの遊ぶ画面です。",
+			icon_url: "../images/minecraft_icon.png",
+			github_counter: "fullscreen-play-screen",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/fullscreen-play-screen/releases/latest"
+		},
+		{
+			name: "SNS Feed Screen",
+			description: "Gote-SupporterにあったSNSみたいなフィード画面です。",
+			icon_url: "../images/minecraft_icon.png",
+			github_counter: "Compact_Feed_Screen",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/Compact_Feed_Screen/releases/latest"
+		},
+		{
+			name: "グリッド表示のサーバーフォーム",
+			description: "サーバーフォームをグリッド表示にします",
+			icon_url: "https://github.com/Gotemba912/grid-sever-form/blob/main/pack_icon.png?raw=true",
+			github_counter: "grid-sever-form",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/grid-sever-form/releases/latest"
+		},
+		{
+			name: "Newline Text for Anvil",
+			description: "金床で改行テキストを使用できます。",
+			icon_url: "https://github.com/Gotemba912/Newline-Text_for_Anvil/blob/main/pack_icon.png?raw=true",
+			github_counter: "Newline-Text_for_Anvil",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/Newline-Text_for_Anvil/releases/latest"
+		},
+		{
+			name: "チャットでセクション装飾消す",
+			description: "チャットでのセクション装飾を消します<br>全てのサーバーで動くわけではありません!",
+			icon_url: "https://github.com/Gotemba912/anti_section_decoration_in_chat/blob/main/pack_icon.png?raw=true",
+			github_counter: "anti_section_decoration_in_chat",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/anti_section_decoration_in_chat/releases/latest"
+		},
+		{
+			name: "World Icon Replacer",
+			description: "ワールドのアイコンを置き換える",
+			icon_url: "https://github.com/Gotemba912/MCBE_WorldIconReplacer/blob/main/pack_icon.png?raw=true",
+			github_counter: "MCBE_WorldIconReplacer",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/MCBE_WorldIconReplacer/releases/latest"
+		},
+		{
+			name: "Tab Key Playerlist",
+			description: "このマイクラ統合版用リソースパックは、<br>HUDでTabキーを押すと、プレイヤーリストを表示することができます。",
+			icon_url: "https://github.com/Gotemba912/Tabkey_Playerlist_UI/blob/main/pack_icon.png?raw=true",
+			github_counter: "Tabkey_Playerlist_UI",
+			github_plus: 0,
+			link: "https://github.com/Gotemba912/Tabkey_Playerlist_UI/releases/latest"
+		}
+	]
+}
