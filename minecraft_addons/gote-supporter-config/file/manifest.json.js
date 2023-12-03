2a08fb9c775bb95f0008aa8b1e01bc8f0418599c
おてんば優香 Yuka0745 <78265558+Gotemba912@users.noreply.github.com>
2023-12-03 16:53:28 +0900

    gf

---
+++ minecraft_addons/gote-supporter-config/file/manifest.json.js
@@ -0,0 +1,18 @@
+let manifestjson = `{
+  "format_version": 2,
+  "header": {
+    "name": "Gote-Supporter Config Pack",
+    "description": "Gote-SupporterをカスタマイズするConfig",
+    "version": [ 1, 0, 0 ],
+    "uuid": "6f21f72d-5c41-4839-a643-3f7f01430ea4",
+    "min_engine_version": [ 1, 13, 0 ],
+    "pack_scope": "global"
+  },
+  "modules": [
+    {
+      "type": "resources",
+      "version": [ 1, 0, 0 ],
+      "uuid": "3e677903-85b8-4b4c-9372-a2c584564fa5"
+    }
+  ]
+}`
