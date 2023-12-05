57adfad93207c0d5ea4375f15529e10b090fd743
おてんば優香 Yuka0745 <78265558+Gotemba912@users.noreply.github.com>
2023-12-05 22:13:18 +0900

    2023年末大掃除！

--- minecraft_addons/z_addon_list.js
+++ minecraft_addons/z_addon_list.js
@@ -1,220 +1,173 @@
-const adlist = {
+let adlist = {
 	addon_list: [
 		{
 			name: "Gote-Supporter",
 			description: "マイクラを快適に便利に楽しめるように設計されたUIリソースパックです!",
-			icon_url: "../images/gote-supporter.png",
-			github_counter: "gote-supporter",
-			github_plus: 127696,
+			icon_url: "https://github.com/Gotemba912/Gote-Supporter/blob/main/pack_icon.png?raw=true",
 			link: "./gote-supporter.html"
 		},
 		{
 			name: "Hidden Item Giver",
 			description: "隠しアイテムを簡単に入手できる!!",
-			icon_url: "../images/hack_item_giver.png",
-			github_counter: "hidden-item-giver",
-			github_plus: 4321,
+			icon_url: "https://github.com/Gotemba912/Hidden-Item-Giver/blob/main/pack_icon.png?raw=true",
 			link: "./hidden_item_giver.html"
 		},
 		{
 			name: "Custom UI Maker",
 			description: "本と羽根ペンを使って内容を自由に入力して表示できるUIを作ることができます!",
-			icon_url: "../images/custom_ui_maker.png",
-			github_counter: "custom-ui-maker",
-			github_plus: 158,
+			icon_url: "https://github.com/Gotemba912/custom-ui-maker/blob/main/pack_icon.png?raw=true",
 			link: "./custom_ui_maker.html"
 		},
 		{
 			name: "Legacy GameConsoleUI",
 			description: "コンソールエディション(WiiU, 旧Switch, PS3, 旧PS4, Xbox360, 旧Xbox One)のようなUIにするリソースパックです!",
-			icon_url: "./images/consoleUI.png",
-			github_counter: "consoleui",
-			github_plus: 0,
+			icon_url: "https://github.com/Gotemba912/ConsoleUI/blob/main/pack_icon.png?raw=true",
 			link: "./console_ui.html"
 		},
 		{
 			name: "Revival to v1.16",
 			description: "変わってしまったUIを1.16風にできる限り戻しました!!!",
 			icon_url: "https://github.com/Gotemba912/revival-to-v1.16/blob/main/pack_icon.png?raw=true",
-			github_counter: "revival-to-v1.16",
-			github_plus: 0,
 			link: "./revival_1.16.html"
 		},
 		{
 			name: "Chat Text Copy",
 			description: "チャットの文章を簡単にコピペできます",
 			icon_url: "https://github.com/Gotemba912/chat-text-copy/blob/main/pack_icon.png?raw=true",
-			github_counter: "chat-text-copy",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/chat-text-copy/releases/latest"
 		},
 		{
 			name: "Preview デバッグテキスト切り替え",
 			description: "Preview版の上のデバッグテキストを非表示にしたりできます。<br>HUD画面でF10を押して切り替えます。",
 			icon_url: "https://github.com/Gotemba912/debug_text_toggle/blob/main/pack_icon.png?raw=true",
-			github_counter: "debug_text_toggle",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/debug_text_toggle/releases/latest"
 		},
 		{
 			name: "タッチ操作でかんたんF5",
 			description: "F5キーをスマホで押せる！<br>camera コマンドの実験を有効化する必要があります。",
 			icon_url: "https://github.com/Gotemba912/touch-f5/blob/main/resource/pack_icon.png?raw=true",
-			github_counter: "touch-f5",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/touch-f5/releases/latest"
 		},
 		{
 			name: "Online Player Count",
 			description: "オンラインのプレイヤー数をポーズ画面に表示します！",
 			icon_url: "https://github.com/Gotemba912/Online-Player-Count/blob/main/pack_icon.png?raw=true",
-			github_counter: "Online-Player-Count",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/Online-Player-Count/releases/latest"
 		},
 		{
 			name: "Custom boss bar UI",
 			description: "カスタムボスバーをscoreboardのsidebarで作れます。<br>/scoreboard objectives add &lt;ID&gt; dummy",
 			icon_url: "https://github.com/Gotemba912/Custom-bossbar-addon/blob/main/pack_icon.png?raw=true",
-			github_counter: "Custom-bossbar-addon",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/Custom-bossbar-addon/releases/latest"
 		},
 		{
 			name: "Fullscreen Bock Screen",
 			description: "Gote-Supporterにあったフルスクリーンの本画面です。",
-			icon_url: "./images/fullscreen_bock_icon.png",
-			github_counter: "fullscreen-bock-screen",
-			github_plus: 0,
+			icon_url: "https://github.com/Gotemba912/fullscreen-bock-screen/blob/main/pack_icon.png?raw=true",
 			link: "https://github.com/Gotemba912/fullscreen-bock-screen/releases/latest"
 		},
 		{
 			name: "Fullscreen Play Screen",
 			description: "Gote-Supporterにあったフルスクリーンの遊ぶ画面です。",
-			icon_url: "../images/minecraft_icon.png",
-			github_counter: "fullscreen-play-screen",
-			github_plus: 0,
+			icon_url: "../textures/minecraft_icon.png",
 			link: "https://github.com/Gotemba912/fullscreen-play-screen/releases/latest"
 		},
 		{
 			name: "SNS Feed Screen",
 			description: "Gote-SupporterにあったSNSみたいなフィード画面です。",
-			icon_url: "../images/minecraft_icon.png",
-			github_counter: "Compact_Feed_Screen",
-			github_plus: 0,
+			icon_url: "../textures/minecraft_icon.png",
 			link: "https://github.com/Gotemba912/Compact_Feed_Screen/releases/latest"
 		},
 		{
 			name: "グリッド表示のサーバーフォーム",
 			description: "サーバーフォームをグリッド表示にします",
 			icon_url: "https://github.com/Gotemba912/grid-sever-form/blob/main/pack_icon.png?raw=true",
-			github_counter: "grid-sever-form",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/grid-sever-form/releases/latest"
 		},
 		{
 			name: "Newline Text for Anvil",
 			description: "金床で改行テキストを使用できます。",
 			icon_url: "https://github.com/Gotemba912/Newline-Text_for_Anvil/blob/main/pack_icon.png?raw=true",
-			github_counter: "Newline-Text_for_Anvil",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/Newline-Text_for_Anvil/releases/latest"
 		},
 		{
 			name: "チャットでセクション装飾消す",
 			description: "チャットでのセクション装飾を消します<br>全てのサーバーで動くわけではありません!",
 			icon_url: "https://github.com/Gotemba912/anti_section_decoration_in_chat/blob/main/pack_icon.png?raw=true",
-			github_counter: "anti_section_decoration_in_chat",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/anti_section_decoration_in_chat/releases/latest"
 		},
 		{
 			name: "World Icon Replacer",
 			description: "ワールドのアイコンを置き換える",
 			icon_url: "https://github.com/Gotemba912/MCBE_WorldIconReplacer/blob/main/pack_icon.png?raw=true",
-			github_counter: "MCBE_WorldIconReplacer",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/MCBE_WorldIconReplacer/releases/latest"
 		},
 		{
 			name: "Tab Key Playerlist",
 			description: "このマイクラ統合版用リソースパックは、<br>HUDでTabキーを押すと、プレイヤーリストを表示することができます。",
 			icon_url: "https://github.com/Gotemba912/Tabkey_Playerlist_UI/blob/main/pack_icon.png?raw=true",
-			github_counter: "Tabkey_Playerlist_UI",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/Tabkey_Playerlist_UI/releases/latest"
 		},
 		{
 			name: "Better Chat Animation",
 			description: "HUDのチャットを良い感じのアニメーションで表示します",
-			icon_url: "../images/minecraft_icon.png",
-			github_counter: "BetterChatAnimation",
-			github_plus: 0,
+			icon_url: "../textures/minecraft_icon.png",
 			link: "https://github.com/Gotemba912/BetterChatAnimation/releases/latest"
 		},
 		{
 			name: "オンスクリーン効果",
 			description: "画面効果を追加します！",
 			icon_url: "https://github.com/Gotemba912/screen-effect/blob/v1.0.0/pack_icon.png?raw=true",
-			github_counter: "screen-effect",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/screen-effect/releases/latest"
 		},
 		{
 			name: "UIを薄くするリソースパック",
 			description: "需要無し・くだらないです。",
 			icon_url: "https://github.com/Gotemba912/usui-ui/blob/main/pack_icon.png?raw=true",
-			github_counter: "usui-ui",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/usui-ui/releases/latest"
 		},
 		{
 			name: "VSync設定追加",
 			description: "v1.20.30でなぜか追加されなかったVSync(垂直同期)設定を追加します!",
 			icon_url: "https://github.com/Gotemba912/vsync-visible/blob/main/pack_icon.png?raw=true",
-			github_counter: "vsync-visible",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/vsync-visible/releases/latest"
 		},
 		{
 			name: "水の音を昔のようにする for v1.20.30",
 			description: "水のサウンドを1.20.10以前のようにします 1.20.30にのみ対応",
 			icon_url: "https://github.com/Gotemba912/fixed-water/blob/main/pack_icon.png?raw=true",
-			github_counter: "fixed-water",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/fixed-water/releases/latest"
 		},
 		{
 			name: "Safezone filler",
 			description: "より小さいGUIにできます！",
 			icon_url: "https://github.com/Gotemba912/safezone-filler/blob/main/pack_icon.png?raw=true",
-			github_counter: "safezone-filler",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/safezone-filler/releases/latest"
 		},
 		{
 			name: "三人称カメラの仕様を戻す",
 			description: "三人称カメラで音をプレイヤー基準の仕様に戻します",
 			icon_url: "https://github.com/Gotemba912/camera-sound-player/blob/main/pack_icon.png?raw=true",
-			github_counter: "camera-sound-player",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/camera-sound-player/releases/latest"
 		},
 		{
 			name: "ブルスクアドオン",
 			description: "ブルースクリーン(BSOD)風の画面を表示できるよ",
 			icon_url: "https://github.com/Gotemba912/bluescreen-addon/blob/main/pack_icon.png?raw=true",
-			github_counter: "bluescreen-addon",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/bluescreen-addon/releases/latest"
 		},
 		{
 			name: "UIパフォーマンス改善",
 			description: "UIでのメモリ使用量を抑え、パフォーマンスを改善します",
 			icon_url: "https://github.com/Gotemba912/memori-sakugen-ui/blob/main/pack_icon.png?raw=true",
-			github_counter: "memori-sakugen-ui",
-			github_plus: 0,
 			link: "https://github.com/Gotemba912/memori-sakugen-ui/releases/latest"
+		},
+		{
+			name: "Item ID Auxを見れるやつ",
+			description: "アイテムのItem ID Auxをインベントリから見れます。<br>タッチ非対応です",
+			icon_url: "https://github.com/Gotemba912/item-id-aux-hyouzi/blob/main/pack_icon.png?raw=true",
+			link: "https://github.com/Gotemba912/item-id-aux-hyouzi/releases/latest"
 		}
 	]
 }
+adlist.addon_list.sort((a, b) => a.name.localeCompare(b.name))
